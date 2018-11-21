a293110b59328e120029165e3435d4a54ba57dbd
Wonderio619 <yaroslav.burlaka12@gmail.com>
2018-11-21 12:34:34 +0200

    Minor additions

--- src/test/java/MobileTest.java
+++ src/test/java/MobileTest.java
@@ -8,7 +8,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
